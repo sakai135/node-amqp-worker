95a8a237dcdf262699e15625b5ebd4d6ef8ded53
sakai135 <sakai135@users.noreply.github.com>
2015-01-20 17:19:31 -0500

    add Client#close()

--- test/client.js
+++ test/client.js
@@ -1,13 +1,56 @@
 var assert = require('assert');
+var amqp = require('amqplib');
 var lib = require('../');
 var Client = lib.Client;
 var Worker = lib.Worker;
 
 describe('Client', function() {
   describe('#connect()', function() {
+
     it('should connect', function(done) {
-      var client = new Client('amqp://localhost');
-      client.connect(done);
+      var client = new Client();
+      client.connect(function(err) {
+        if (err) {
+          done(err);
+        }
+        client.close(done);
+      });
     });
+
+    it('should error on wrong config', function(done) {
+      var client = new Client('amqp://wrong');
+      client.connect(function(err) {
+        if (err) {
+          done();
+        } else {
+          done(new Error('should have error\'ed'));
+        }
+      });
+    });
+
+    it('should emit complete on worker message completion', function(done) {
+      amqp.connect().then(function(conn) {
+        return conn.createChannel();
+      }).then(function(chan) {
+        chan.sendToQueue('demo', new Buffer(JSON.stringify({
+          hi: 'test'
+        })));
+        chan.close();
+      });
+
+      var client = new Client();
+      client.addWorker(new Worker('demo', function(msg, callback) {
+        callback();
+      }));
+      client.connect(function() {});
+      client.on('complete', function(data) {
+        if (data.err) {
+          done(data.err);
+        } else {
+          client.close(done);
+        }
+      });
+    });
+
   });
 });
